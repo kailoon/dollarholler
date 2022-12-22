943f0e2ca80aef428e85585754323398b60401dc
kailoon <chankailoon@gmail.com>
2022-12-22 18:51:02 +0800

    add thank you page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,8 @@ module.exports = {
 				arrowLefttUp: "url('/images/arrow-left-up.svg')",
 				arrowRightUp: "url('/images/arrow-right-up.svg')",
 				chevronDown: "url('/images/chevron-down.svg')",
-				circle: "url('/images/circle.svg')"
+				circle: "url('/images/circle.svg')",
+				thankYou: "url('/images/bg-thankYou.svg')"
 			},
 			backgroundPosition: {
 				'right-center': '97% center'
