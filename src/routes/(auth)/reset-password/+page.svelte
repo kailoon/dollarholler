9bde7e0cec9088ea162d3ad40f2f7956720744b7
kailoon <chankailoon@gmail.com>
2023-01-18 21:16:03 +0800

    signup flow pages

---
+++ src/routes/(auth)/reset-password/+page.svelte
@@ -0,0 +1,18 @@
+<h1 class="auth-heading">Reset My Password</h1>
+
+<form>
+	<div class="field">
+		<label for="newPassword" class="text-goldenFizz">New Password</label>
+		<input type="password" name="newPassword" id="newPassword" />
+	</div>
+
+	<div class="field">
+		<label for="conformPassword" class="text-goldenFizz">Confirm Password</label>
+		<input type="password" name="conformPassword" id="conformPassword" />
+	</div>
+
+	<button type="submit" class="auth-button">Update my password!</button>
+	<p class="mt-4 text-center text-sm text-white">
+		<a href="/login" class="hover-no-underline underline">I'm ready to login.</a>
+	</p>
+</form>
