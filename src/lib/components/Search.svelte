7a9e80dd8c442d267fdd716cc12e27de34f633c5
kailoon <chankailoon@gmail.com>
2023-01-19 11:39:34 +0800

    search button animation

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -10,8 +10,25 @@
 		type="text"
 		name="search"
 		placeholder="Search by keyword"
-		class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] lg:w-72 lg:text-xl"
+		class="search w-full border-b-2 border-dashed border-b-pastelPurple bg-transparent pr-[80px] font-sansSerif text-base text-black outline-none focus:border-solid focus:border-b-lavenderIndigo md:w-[14rem] md:pr-0 lg:w-72 lg:text-xl"
 	/>
-	<button class="font-sansSerif text-lg font-black text-pastelPurple lg:text-xl">Search</button>
-	<!-- new invoice button -->
+	<button
+		class="pointer-events-none absolute left-8 transform font-sansSerif text-lg font-black text-pastelPurple transition-transform lg:text-xl"
+		>Search</button
+	>
 </div>
+
+<style lang="postcss">
+	::placeholder {
+		color: transparent;
+	}
+	input:not(:placeholder-shown) + button,
+	input:focus + button {
+		@apply pointer-events-auto left-auto right-0 translate-x-[calc(100%-65px)] text-right text-lavenderIndigo md:right-auto md:left-0 md:translate-x-[16rem] lg:translate-x-80;
+	}
+
+	input:focus + button:hover,
+	input:focus + button:focus {
+		@apply text-daisyBush;
+	}
+</style>
